1390c0850ed744beae9deb2cdcc2fd94f51cb208
RadonZ <supachai27102000@gmail.com>
2024-05-25 15:33:39 +0700

    update: Post card

--- src/app/_components/PostCard.tsx
+++ src/app/_components/PostCard.tsx
@@ -8,7 +8,7 @@ export default function PostCard() {
     <div className="p-8 rounded-lg shadow-md max-w-md bg-white">
       <div className="flex items-center justify-between mb-4">
         <div className="flex items-center space-x-2">
-        <Image src='next.svg' width={32} height={32} alt="User Avatar" className='rounded-full' />
+        <Image src='/dummy-avatar.jpg' width={32} height={32} alt="User Avatar" className='rounded-full' />
         <div>
           <p className="text-gray-800 font-semibold">John Doe</p>
           <p className="text-gray-500 text-sm">Posted 2 hours ago</p>
@@ -21,20 +21,34 @@ export default function PostCard() {
         </div>
       </div>
       <div className="mb-4">
-        <Image src="next.svg" width={2000} alt="Post Image"  height={48} className="object-cover rounded-md"/>
+        <Image src="/olympics-blog.webp" width={2000} alt="Post Image"  height={48} className="object-cover rounded-md"/>
         </div>
         <div className="mb-4">
           <p>Let know about paris olympics together</p>
       </div>
       <div className="flex items-center justify-between text-gray-500">
         <div className="flex items-center space-x-2">
-          <button>
-
+          <button className="flex justify-center items-center gap-2 px-2 hover:bg-gray-300 rounded-full p-1">
+            <Image src="heart-svgrepo-com.svg" width={32} height={32} alt="like"/>
+            <span>40 Likes</span>
           </button>
         </div>
-      <button></button>
+      <button className="flex justify-center items-center gap-2 px-2 hover:bg-gray-300 rounded-full p-1">
+      <Image src="/comments.png" width={32} height={32} alt="comment"/>
+      <span>1 comment</span>
+      </button>
       </div>
-      </div>
+      <hr className="mt-2 mb-2"/>
+      <h2>Comment</h2>
+      <hr className="mt-2 mb-2"/>
+      <div className="flex items-center space-x-2 mt-2 ml-6">
+      <Image src="/dummy.png" alt="avatar image" width={32} height={32}  className='rounded-full'/>
+      <div>
+        <h3>Eddy Jone</h3>
+        <p>Great Knowledge for Olympics</p>
+      </div> 
+    </div>
+  </div>
     
   )
 }
