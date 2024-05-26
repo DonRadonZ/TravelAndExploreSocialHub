30a2445680068b88593dc443dce71026e8ab4fee
RadonZ <supachai27102000@gmail.com>
2024-05-26 22:51:29 +0700

    update: profile page

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -4,19 +4,20 @@ import PostCard from '../_components/PostCard'
 
 export default function Profile() {
   return (
-    <div className=' lg:container sm:mx-auto px-5 sm:px-10 xl:px-20 pt-24 items-center'>
+    <div className=' lg:container sm:mx-auto px-5 sm:px-10 xl:px-20 pt-10 items-center'>
       <div className="flex items-center gap-3 sm:gap-5 max-sm:place-items-center">
-        <div className="size-32 bg-primary rounded-full bg-white items-center">
-        <h1 className="mt-12 ml-10">Photo</h1>
+        <div className="size-48 bg-primary rounded-full bg-white items-center ml-10">
+        <Image src='/dummy-avatar.jpg' width={256} height={64} alt="User Avatar" className='rounded-full' />
+        <div></div>
         </div>
       </div> 
 
-    <div className="text-base-100 flex flex-col gap-3 max-sm:items-center"> 
+    <div className="text-base-100 flex flex-col gap-3 max-sm:items-center ml-10"> 
     <h1 className="p-2 px-2 text-3xl font-bold"> 
-      RadonZ
+      John Doe
     </h1>
     </div> 
-    <div className="grid grid-cols-3 gap-3 text-xl text-base-100 w-fit ">
+    <div className="grid grid-cols-3 gap-3 text-xl text-base-100 w-fit ml-12 ">
     <div className="flex items-center text-center gap-2">
       <strong>200</strong>
       <span> Following</span>
@@ -26,7 +27,7 @@ export default function Profile() {
       <span> Follower</span>
     </div>
     <div>
-      <strong>4</strong>
+      <strong>3</strong>
       <span> Post</span>
     </div>
     </div>
